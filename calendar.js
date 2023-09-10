a5b68fc26affc4dcb99156f05e6ed6d9acd78133
Sadat Saif <sadat.saif@ucalgary.ca>
2023-09-09 21:31:11 -0600

    Users can now add custom events

--- calendar.js
+++ calendar.js
@@ -2,17 +2,36 @@ document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "dayGridMonth", // Display the calendar in month view
-    events: [
-      //Random place holder events
-      {
-        title: "Event 1",
-        start: "2023-09-10",
-      },
-      {
-        title: "Event 2",
-        start: "2023-09-15",
-      },
-    ],
+    selectable: true, // Allow date selection
+    select: function (info) {
+      // When a date is clicked
+      openCustomEvent(info.startStr); // Open the event input form
+    },
   });
   calendar.render();
+  function openCustomEvent(date) {
+    // Display the event input form
+    document.getElementById("eventDate").value = date;
+    document.getElementById("customEvent").style.display = "block";
+  }
+
+  function closeCustomEvent() {
+    // Close the event input form
+    document.getElementById("customEvent").style.display = "none";
+  }
+
+  document.getElementById("eventForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const eventTitle = document.getElementById("eventTitle").value;
+    const eventDate = document.getElementById("eventDate").value;
+
+    if (eventTitle && eventDate) {
+      calendar.addEvent({
+        title: eventTitle,
+        start: eventDate,
+      });
+
+      closeCustomEvent(); // Close the event input form
+    }
+  });
 });
