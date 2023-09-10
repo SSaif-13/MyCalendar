05250aabd20c5750319178c00f657b2a2b96d90a
Sadat Saif <sadat.saif@ucalgary.ca>
2023-09-09 21:40:19 -0600

    Cleaned up the code to make it look simpler

--- calendar.js
+++ calendar.js
@@ -9,29 +9,4 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
   calendar.render();
-  function openCustomEvent(date) {
-    // Display the event input form
-    document.getElementById("eventDate").value = date;
-    document.getElementById("customEvent").style.display = "block";
-  }
-
-  function closeCustomEvent() {
-    // Close the event input form
-    document.getElementById("customEvent").style.display = "none";
-  }
-
-  document.getElementById("eventForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const eventTitle = document.getElementById("eventTitle").value;
-    const eventDate = document.getElementById("eventDate").value;
-
-    if (eventTitle && eventDate) {
-      calendar.addEvent({
-        title: eventTitle,
-        start: eventDate,
-      });
-
-      closeCustomEvent(); // Close the event input form
-    }
-  });
 });
